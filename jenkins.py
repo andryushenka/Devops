80034d7b3dc0ac940d8e471bf25e2e4af9df8316
andryushenka <93209829+andryushenka@users.noreply.github.com>
2021-11-24 13:42:10 +0300

    Update jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -9,4 +9,4 @@ def hello_world():
     return 'Hello World port 4000'
  
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=4000)
+    app.run(debug=True,host='127.0.0.1',port=4000)
